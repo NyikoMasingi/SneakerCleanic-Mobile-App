21d8fbda26130cf5b4da1b53551c315e8f60ecfe
yinhla <makamuyinhla2016@gmail.com>
2024-10-05 20:21:49 +0200

    created login page

--- lib/screens/authentication/LoginScreen.dart
+++ lib/screens/authentication/LoginScreen.dart
@@ -29,49 +29,181 @@ class _LoginScreenState extends State<LoginScreen> {
     double w = MediaQuery.of(context).size.width;
     return Stack(
       children: [
-        Scaffold(
-          //appBar: CupertinoNavigationBar(),
-          body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: w * 0.05, vertical: h * 0.05),
+        CupertinoPageScaffold(
+          resizeToAvoidBottomInset: true,
+          child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                CupertinoTextField(
-                  controller: phoneNumberEditingController,
-                  placeholder: "Phone Number",
-                  maxLength: 10,
-                ),
-                SizedBox(height: h * 0.03,),
-                CupertinoTextField(
-                  controller: passEditingController,
-                  placeholder: "Password",
-                  obscureText: true,
-                ),
-                SizedBox(height: h * 0.03,),
-                CupertinoButton(
-                  color: Colors.blue,
-                  onPressed: (){
-                    userLogin(context);
+              children: [
+                SizedBox(
+                  height: h * 0.38,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            scale: 6,
+                            image: AssetImage(
+                                'assets/logo.png'
+                            )
+                        ),
+                      color: Colors.black,
+                      shape: BoxShape.rectangle
+                    ),
 
-                  },
-                  child: const Text(
-                    "Login",
-                    style: TextStyle(
-                      color: Colors.white
+                  ),
+                ),
+                Container(
+                  height: h* 0.1,
+                  alignment: Alignment.center,
+                  child: const Column(
+                    children: [
+                      Text(
+                        'SNEAKER',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.black,
+                          decoration: TextDecoration.none
+                        ),
+                      ),
+                      Text(
+                        'CLEANIC',
+                        style: TextStyle(
+                          fontSize: 24,
+                          decoration: TextDecoration.none
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: h * 0.15,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: w * 0.05),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: const BorderRadius.all(Radius.circular(50)),
+                              border: Border.all(
+                                width: 1,
+                                color: const Color.fromRGBO(255, 80, 80 ,0.7),
+                              )
+                          ),
+                          child: CupertinoListTile(
+                            leading: const Icon(
+                              Icons.person,
+                              color: Color.fromRGBO(255, 80, 80 ,0.5),
+                            ),
+                            title: CupertinoTextField(
+                              decoration: const BoxDecoration(),
+                              controller: phoneNumberEditingController,
+                              placeholder: "Email or Phone",
+                              maxLength: 10,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: h * 0.02,),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.all(Radius.circular(50)),
+                            border: Border.all(
+                              width: 1,
+                              color: const Color.fromRGBO(255, 80, 80 ,0.7),
+                            )
+                          ),
+                          child: CupertinoListTile(
+                            leading: const Icon(
+                              Icons.lock,
+                              color: Color.fromRGBO(255, 80, 80 ,0.7),
+                            ),
+                            title: CupertinoTextField(
+                              decoration: const BoxDecoration(),
+                              controller: passEditingController,
+                              placeholder: "Password",
+                              obscureText: true,
+                            ),
+                          ),
+                        ),
+
+                      ],
                     ),
                   ),
-                )
+                ),
+                SizedBox(height: h * 0.04,),
+                Container(
+                  height: h * 0.34,
+                  width: w,
+                  decoration: const BoxDecoration(
+                    color: Color.fromRGBO(189, 7, 7, 1),
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(50), topRight: Radius.circular(50))
+            
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        TextButton(
+                          onPressed: (){},
+                          child: const Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: h * 0.01,),
+                        SizedBox(
+                          width: w * 0.7,
+                          height: h * 0.06,
+                          child: CupertinoButton(
+                            color: Colors.white,
+                            onPressed: (){
+            
+                            },
+                            child: const Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+            
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: h * 0.02,),
+                        const Text(
+                          'or',
+                          style: TextStyle(
+                            color: Colors.white,
+                            decoration: TextDecoration.none,
+                            fontSize: 18
+                          ),
+                        ),
+                        SizedBox(height: h * 0.02,),
+                        SizedBox(
+                          width: w * 0.7,
+                          height: h * 0.06,
+                          child: CupertinoButton(
+                            color: Colors.grey,
+                            onPressed: (){},
+            
+                            child: const Text(
+                              'Create an account',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
         ),
-        if(_isLoading)
-          Container(
-            height: h,
-            width: w,
-            color: Colors.black.withOpacity(0.5),
-            child: const CupertinoActivityIndicator(),
-          )
+
       ],
     );
   }
